88eb8a018a3aa85b746d574371c55eef8df90fcc
Charlie Windolf <cwindolf95@gmail.com>
2015-05-01 22:16:02 -0400

    pond v2 - somewhat less boring

--- pond.js
+++ pond.js
@@ -7,12 +7,13 @@ function PondPoint (x, v) {
 // requires paper.js
 function Pond ($ctnr) {
 	var self = this; // <3 JS!
-	this.INSET = 10;
-	this.E = .8;
+	this.INSET = 50;
+	this.E = .2;
 	this.$ctnr = $ctnr;
 	this.width = Math.round($ctnr.width());
 	this.height = Math.round($ctnr.height());
 	this.canvas = document.createElement("canvas");
+	$(this.canvas).css("background-color", "#006699");
 	this.canvas.setAttribute("id", "pond");
 	this.canvas.setAttribute("width", this.width);
 	this.canvas.setAttribute("height", this.height);
@@ -27,18 +28,24 @@ function Pond ($ctnr) {
 	var spoint = new paper.Point(this.INSET,this.INSET);
 	var ssize = new paper.Size(self.width - this.INSET * 2, self.height - this.INSET * 2);;
 	this.shore = new paper.Path.Rectangle(spoint, ssize);
-	this.shore.strokeColor = 'black';
+	this.shore.visible = false;
+
+	// this.bg = new paper.Path.Rectangle(paper.view.bounds);
+	// this.bg.fillColor = "blue";
 
 	this.render = function() {
 		this.pathsGroup.removeChildren();
 
 		for (var i = 0; i < this.ripples.length; i++) {
 			var path = new paper.Path();
-			path.strokeColor = "black";
+			path.strokeColor = "white";
 			for (var p = 0; p < this.ripples[i].length; p++) {
 				path.add(this.ripples[i][p].x);
 			}
 			path.closed = true;
+			path.strokeColor = "white";
+			path.fillColor = "#dee";
+			path.opacity = 0.2;
 			path.smooth();
 			this.pathsGroup.addChild(path);
 		}
@@ -49,32 +56,37 @@ function Pond ($ctnr) {
 			var pt;
 			for (var p = 0; p < this.ripples[i].length; p++) {
 				pt = this.ripples[i][p];
+				// accelerate away from shore edges
 				if (!this.shore.contains(pt.x)) {
-					// flip y-velocity
-					if (pt.x.y <= this.INSET || pt.x.y >= this.height - this.INSET * 2) {
-						pt.v.y = -this.E * pt.v.y;
-						pt.x.y += 0.1 * pt.v.y;
+					if (pt.x.y <= this.INSET) {
+						pt.v.y += this.E;
+					} else if (pt.x.y >= this.height - this.INSET * 2) {
+						pt.v.y -= this.E;
 					}
-					// flip x-velocity
-					if (pt.x.x <= this.INSET || pt.x.x >= this.width - this.INSET * 2) {
-						pt.v.x = -this.E * pt.v.x;
-						pt.x.x += 0.1 * pt.v.x;
+					if (pt.x.x <= this.INSET) {
+						pt.v.x += this.E;
+					} else if (pt.x.x >= this.width - this.INSET * 2) {
+						pt.v.x -= this.E;
 					}
+					pt.v = pt.v.multiply(0.99);
 				}
 				pt.x = pt.x.add(pt.v.multiply(dt / 1000)); //ms
 			}
 		}
 	};
 
-	this.time = Date.now();
-	paper.view.on('frame', function() {
+	this.update = function() {
 		if (self.ripples.length > 0) {
 			var dt = Date.now() - self.time;
 			self.step(dt);
 			self.render();
 			self.time = Date.now();
 		}
-	});
+	};
+
+	this.time = Date.now();
+	paper.view.on('frame', this.update);
+	// setInterval(this.update, 100);	
 
 	this.tool = new paper.Tool();
 	this.tool.onMouseDown = function(event) {
@@ -82,7 +94,7 @@ function Pond ($ctnr) {
 		self.ripples.push([]);
 		var pt;
 		// surround event.point with path points
-		for (var theta = 0; theta < 2* Math.PI; theta += Math.PI/128) {
+		for (var theta = 0; theta < 2* Math.PI; theta += Math.PI/64) {
 			pt = new paper.Point(Math.cos(theta), Math.sin(theta));
 			var ppt = new PondPoint(event.point.add(pt), pt.multiply(30));
 			self.ripples[self.ripples.length - 1].push(ppt);
